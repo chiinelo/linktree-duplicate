3fbe1ffca0806f20e5a0e16865d490f61999a060
Chinelo Obigwe <obigwechinelo@gmail.com>
2022-10-24 15:45:42 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import landingPage from "../src/styles/landingPage.css";
 function App() {
   return (
     <div className="App">
-      <div className="landing-page container-fluid">
+      <div className="landing-page">
+        <div>
+          <img
+            src="../images/_Avatar share button1.svg"
+            alt=""
+            className="desktop-share d-none d-md-block"
+          />
+          <img
+            src="../images/_Avatar share button.svg"
+            alt=""
+            className="mobile-toggle"
+          />
+        </div>
         <div className="profile">
           <img
-            src="../images/profile__img.svg"
+            src="../images/IMG_1404.JPG "
             alt="profile-img"
             id="profile__img"
+
           />
           <p id="twitter" className="profile_name">
-            Annette Black
+            Chinelo Obigwe
           </p>
         </div>
         <div className="links-group">
           <button className="links">
-            <a href="" className="link-texts">
+            <a href="https://twitter.com/chiinelo_" className="link-texts">
               Twitter Link
             </a>
           </button>
@@ -50,6 +63,20 @@ function App() {
               Design Books
             </a>
           </button>
+          <div className="socials d-none d-md-block">
+            <img src="../images/slack.svg" id="slack" className="slack" />
+            {/* slack name = chinelo.obigwe@yahoo.com */}
+            <img src="../images/Social icon.svg" alt="" />
+          </div>
+          <div className="d-none d-md-block">
+            <hr></hr>
+
+            <div className="d-flex justify-content-between pb-5 pt-0">
+              <img src="../images/logo.36d2d48a.svg" alt="slack-logo" />
+              <p>HNG Internship 9 Frontend Task</p>
+              <img src="../images/I4G.svg" alt="github-logo" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
