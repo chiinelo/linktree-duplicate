f8b371781bc8462fd930c41ce2eb8ca85b484068
Chinelo Obigwe <chineloobigwe@Chinelos-MacBook-Pro.local>
2022-11-05 15:29:42 +0100

    fully responsive

--- src/Contact.js
+++ src/Contact.js
@@ -3,37 +3,43 @@ import  "../src/styles/contactPage.css";
 
 const contact = () => {
   return (
+
+<>
     <div className='contact'>
 
     <div className='landing-page'>
         <h1 className='fs-1 fw-bolder'>Contact Me</h1>
         <p className='fs-4 fw-lighter'>Hi there, contact me to ask me about anything you have in mind.</p>
     </div>
+<div className='first-lastName'>
 
-    <form >
+    <form className='form-Name'>
         <label>First Name</label> <br />
         <input type="text" placeholder='Enter your first name' id='first_name' className='name-email-input'/>
     </form>
-    <form >
+    <form className='form-Name'>
         <label>Last Name</label> <br />
         <input type="text" placeholder='Enter your last name' id='last_name' className='name-email-input'/>
     </form>
+</div>
+
     <form >
         <label>Email</label> <br />
-        <input type="email" placeholder='yourname@email.com' id='email'  className='name-email-input'/>
+        <input type="email" placeholder='yourname@email.com' id='email'  className='email-input'/>
     </form>
     <form >
         <label>Message</label> <br />
         <textarea type="text" placeholder="Send me a message and I'll reply you as soon s possible" id='message' className='message-input'/>
     </form>
     <label for="vehicle1" className=''>
-    <input type="checkbox"/>
+    <input type="checkbox" className='me-2 checkbox-input'/>
    You agree to providing your data to who may contact you.</label>
 
 
     <button id='btn__submit' className='message-button mb-3'>Send message</button>
     
-          <div className="mt-5">
+    </div>
+          <div className="mt-5 p-5">
             <hr></hr>
 
             <div className="pb-5 pt-0 logo-footer">
@@ -42,7 +48,9 @@ const contact = () => {
               <img src="../images/I4G.svg" alt="github-logo" />
             </div>
           </div>
-    </div>
+
+
+          </>
 
   )
 }
